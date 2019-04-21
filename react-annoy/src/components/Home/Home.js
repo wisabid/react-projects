3bcbcef97f231f6c7ff421b71e82a75e75f0bade
Abid Shamim P.M <abid.shamim.p.m@ing.com>
2019-04-21 02:47:14 +0200

    chore(app):new app

---
+++ react-annoy/src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './home.css';
+
+const Home = (props) => {
+    return (
+        <section className="annoy-container">
+            <header>Im a header</header>
+            <div>dfgdfgdfg</div>
+            <div>dfgdfgdfg</div>
+            <div>dfgdfgdfg</div>
+            <div className="msg-box">sdffdgdffd</div>
+        </section>
+    )
+}
+
+export default Home;
